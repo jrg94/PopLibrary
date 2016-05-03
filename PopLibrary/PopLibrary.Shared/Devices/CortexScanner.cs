e3d6b3c1583ee6a3d06c3cdea305dbf159416c9f
jrg94 <jrg170@case.edu>
2016-05-02 23:42:11 -0400

    Added a small rant to the device code

--- PopLibrary/PopLibrary.Shared/Devices/CortexScanner.cs
+++ PopLibrary/PopLibrary.Shared/Devices/CortexScanner.cs
@@ -4,6 +4,18 @@ using System.Text;
 
 namespace PopLibrary.Devices
 {
+    /// <summary>
+    /// Represents a barcode scanner device
+    /// Some advice: Do not purchase this scanner unless
+    /// you literally only want to enter barcodes into a spreadsheet
+    /// Using this thing as been a complete and total nightmare because
+    /// it is not HID-Scanner compliant
+    /// The computer thinks its a keyboard and it uses pages that
+    /// are blocked in the HIDDevice library in .NET
+    /// As a result, you have to use RawInput to handle it which
+    /// basically means writing threads at near C level code to 
+    /// handle the keyboard data stream and the barcode data stream
+    /// </summary>
     class CortexScanner
     {
         public const UInt16 Vid = 0x05FE;
