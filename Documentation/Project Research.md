252ea553e96b7d938ae83473f00755f8c7631dcb
jrg94 <jrg170@case.edu>
2016-05-05 19:35:45 -0400

    Committing lots of broken code
    The Amazon API is proving to be a disaster. I'm not trying to pay for
    the service, and I can't seem to get all of the libraries to work just
    right. I'm either having .NET issues or I just can't seem to find the
    solution online. So that's been fun.

--- Documentation/Project Research.md
+++ Documentation/Project Research.md
@@ -10,3 +10,6 @@
 		* Purchase a new scanner which allows me to use the libraries built for Barcode Scanners
 	* So it turns out I can't use the HidDevice library either because the device uses the usage page 0001 - 0006 which are apparently banned by the library - as a result I suppose I'll have to look at one of the other solutions above - I'm thinking either raw data separation or buying a new scanner
 * I am using Visual Studio to build a Windows 8.1 application – XAML is proving to be annoying already, but I have been watching tutorials on how to setup a Universal project (assuming I want to port this to a Windows phone)
+# Gameplan
+* The plan is to now just skip automation with the scanner - the UI will have a text box which we can scan codes into and the user can hit search with it
+* The next challenge is to actually have that search hit a server which holds the UPC codes - I'm currently running into major issues with the Amazon API
--- PopLibrary/PopLibrary.Windows/MainPage.xaml.cs
+++ PopLibrary/PopLibrary.Windows/MainPage.xaml.cs
@@ -1,20 +1,7 @@
-﻿using PopLibrary.ClassLibrary.DataClasses;
+﻿using PopLibrary.com.amazon.webservices;
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -25,6 +12,26 @@ namespace PopLibrary
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        // Use one of the following destinations, according to the region you are
+        // interested in:
+        // 
+        //      US: ecs.amazonaws.com 
+        //      CA: ecs.amazonaws.ca 
+        //      UK: ecs.amazonaws.co.uk 
+        //      DE: ecs.amazonaws.de 
+        //      FR: ecs.amazonaws.fr 
+        //      JP: ecs.amazonaws.jp
+        //
+        // Note: protocol must be https for signed SOAP requests.
+        const String DESTINATION = "https://ecs.amazonaws.com/onca/soap?Service=AWSECommerceService";
+
+        // Set your AWS Access Key ID and AWS Secret Key here.
+        // You can obtain them at:
+        // http://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key
+        const String MY_AWS_ID = "AKIAJTPD7BGTWVVQLASQ";
+        const String MY_AWS_SECRET = "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";
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -33,7 +40,28 @@ namespace PopLibrary
 
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
-            barcodeBox.Text = "Jamal";
+            System.Diagnostics.Debug.WriteLine("BookSearch v1.0");
+
+            AWSECommerceService service = new AWSECommerceService();
+
+            ItemSearch request = new ItemSearch();
+
+            request.SubscriptionId = "0525E2PQ81DD7ZTWTK82";
+
+            request.Request = new ItemSearchRequest[1];
+            request.Request[0] = new ItemSearchRequest();
+            request.Request[0].ResponseGroup = new string[] { "Small" };
+            request.Request[0].SearchIndex = "Books";
+            request.Request[0].Author = "Tom Clancy";
+
+            ItemSearchResponse response = service.ItemSearch(request);
+
+            System.Diagnostics.Debug.WriteLine(response.Items[0].Item.Length + " books written by Tom Clancy found.");
+        }
+
+        private void submitButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
