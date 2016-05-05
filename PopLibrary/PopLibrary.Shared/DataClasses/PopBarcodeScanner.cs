d6ebca63b302d142e5c774384e59c4f218446516
jrg94 <jrg170@case.edu>
2016-05-05 19:53:08 -0400

    Starting fresh
    Removed a lot of the bullshit code from attempting to use Amazon's API

--- PopLibrary/PopLibrary.Shared/DataClasses/PopBarcodeScanner.cs
+++ PopLibrary/PopLibrary.Shared/DataClasses/PopBarcodeScanner.cs
@@ -6,20 +6,6 @@ namespace PopLibrary.ClassLibrary.DataClasses
 {
     public class PopBarcodeScanner
     {
-        #region Raw Input API
 
-        [DllImport("User32.dll")]
-        extern static uint GetRawInputDeviceList(IntPtr pRawInputDeviceList, ref uint uiNumDevices, uint cbSize);
-
-        [DllImport("User32.dll")]
-        extern static uint GetRawInputDeviceInfo(IntPtr hDevice, uint uiCommand, IntPtr pData, ref uint pcbSize);
-
-        ///[DllImport("User32.dll")]
-        ///extern static bool RegisterRawInputDevices(RAWINPUTDEVICE[] pRawInputDevice, uint uiNumDevices, uint cbSize);
-
-        [DllImport("User32.dll")]
-        extern static uint GetRawInputData(IntPtr hRawInput, uint uiCommand, IntPtr pData, ref uint pcbSize, uint cbSizeHeader);
-
-        #endregion
     }
 }
--- PopLibrary/PopLibrary.Windows/MainPage.xaml.cs
+++ PopLibrary/PopLibrary.Windows/MainPage.xaml.cs
@@ -12,24 +12,6 @@ namespace PopLibrary
     /// </summary>
     public sealed partial class MainPage : Page
     {
-
-        // Use one of the following destinations, according to the region you are
-        // interested in:
-        // 
-        //      US: ecs.amazonaws.com 
-        //      CA: ecs.amazonaws.ca 
-        //      UK: ecs.amazonaws.co.uk 
-        //      DE: ecs.amazonaws.de 
-        //      FR: ecs.amazonaws.fr 
-        //      JP: ecs.amazonaws.jp
-        //
-        // Note: protocol must be https for signed SOAP requests.
-        const String DESTINATION = "https://ecs.amazonaws.com/onca/soap?Service=AWSECommerceService";
-
-        // Set your AWS Access Key ID and AWS Secret Key here.
-        // You can obtain them at:
-        // http://aws-portal.amazon.com/gp/aws/developer/account/index.html?action=access-key
-
         public MainPage()
         {
             this.InitializeComponent();
@@ -38,28 +20,6 @@ namespace PopLibrary
 
         private void SubmitButton_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("BookSearch v1.0");
-
-            AWSECommerceService service = new AWSECommerceService();
-
-            ItemSearch request = new ItemSearch();
-
-            request.SubscriptionId = "0525E2PQ81DD7ZTWTK82";
-
-            request.Request = new ItemSearchRequest[1];
-            request.Request[0] = new ItemSearchRequest();
-            request.Request[0].ResponseGroup = new string[] { "Small" };
-            request.Request[0].SearchIndex = "Books";
-            request.Request[0].Author = "Tom Clancy";
-
-            ItemSearchResponse response = service.ItemSearch(request);
-
-            System.Diagnostics.Debug.WriteLine(response.Items[0].Item.Length + " books written by Tom Clancy found.");
-        }
-
-        private void submitButton_Click_1(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
