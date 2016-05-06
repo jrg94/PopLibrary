3d07e6fdea23cf020570b582f7aee103329736db
jrg94 <jrg170@case.edu>
2016-05-06 16:30:51 -0400

    Added some query code

--- PopLibrary/PopLibrary.Shared/DatabasesClasses/PopLibraryDataManager.cs
+++ PopLibrary/PopLibrary.Shared/DatabasesClasses/PopLibraryDataManager.cs
@@ -49,30 +49,27 @@ namespace PopLibrary.DatabasesClasses
         /// <param name="student"></param>
         /// <param name="book"></param>
         /// <param name="loan"></param>
-        public async void LoanBook(Student student, Loan loan)
+        public void LoanBook(Student student, Loan loan)
         {
-            SQLiteAsyncConnection conn = new SQLiteAsyncConnection("PopLibrary.db");
-            await conn.InsertAsync(student);
-            await conn.InsertAsync(loan);
+            InsertAsync(student);
+            InsertAsync(loan);
         }
 
         /// <summary>
         /// Handles behavior when a student returns a book
         /// </summary>
         /// <param name="loan"></param>
-        public async void ReturnBook(Loan loan)
+        public void ReturnBook(Loan loan)
         {
-            SQLiteAsyncConnection conn = new SQLiteAsyncConnection("PopLibrary.db");
-            await conn.UpdateAsync(loan);
+            UpdateAsync(loan);
         }
 
         /// <summary>
         /// A function for adding a book to the database
         /// </summary>
-        public async void AddBook(Book book)
+        public void AddBook(Book book)
         {
-            SQLiteAsyncConnection conn = new SQLiteAsyncConnection("PopLibrary.db");
-            await conn.InsertAsync(book);
+            InsertAsync(book);
         }
     }
 }
