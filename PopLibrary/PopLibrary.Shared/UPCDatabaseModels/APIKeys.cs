ce4b2108ad4a6c0d8e43d79848af06a79771100c
jrg94 <jrg170@case.edu>
2016-05-06 22:06:12 -0400

    Added an api keys object
    Used to pull hidden keys from local config files so data doesn't show up
    on github

---
+++ PopLibrary/PopLibrary.Shared/UPCDatabaseModels/APIKeys.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PopLibrary.UPCDatabaseModels
+{
+    class APIKeys
+    {
+        public string upcDatabaseKey { get; set; }
+        public string digitEyesAppKey { get; set; }
+        public string digitEyesAuthorizationKey { get; set; }
+    }
+}
