e2ff5b1a0210af145670d5489c8671a9cdc12756
jrg94 <jrg170@case.edu>
2016-05-06 21:42:40 -0400

    Modeling Digit Eyes

--- PopLibrary/PopLibrary.Shared/DigitEyesObject.cs
+++ PopLibrary/PopLibrary.Shared/DigitEyesObject.cs
@@ -16,11 +16,34 @@ namespace PopLibrary
         public string website { get; set; }
         public string product_web_page { get; set; }
         public string image { get; set; }
+        public Thumbnail thumnbnail { get; set; }
         public Manufacturer manufacturer { get; set; } 
+        public GCP gcp { get; set; }
+        public string categories { get; set; }
+
+        public struct Thumbnail
+        {
+            public string thumbnail_url { get; set; }
+            public string thumbnail_width { get; set; }
+            public string thumbnail_height { get; set; }
+        }
         
         public struct Manufacturer
         {
             public string name { get; set; }
+            public string address { get; set; }
+            public string address2 { get; set; }
+            public string city { get; set; }
+            public string state { get; set; }
+            public string country { get; set; }
+            public string postal_code { get; set; }
+            public string contact { get; set; }
+            public string phone { get; set; }
+        }
+
+        public struct GCP
+        {
+            public string company { get; set; }
             public string address { get; set; }
             public string address2 { get; set; }
             public string city { get; set; }
