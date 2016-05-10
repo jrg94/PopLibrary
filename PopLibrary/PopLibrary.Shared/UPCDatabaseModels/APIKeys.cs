eca7574a066f9f842f307b4540bc13e92f7990e5
jrg94 <jrg170@case.edu>
2016-05-10 00:10:59 -0400

    Added config file reading

--- PopLibrary/PopLibrary.Shared/UPCDatabaseModels/APIKeys.cs
+++ PopLibrary/PopLibrary.Shared/UPCDatabaseModels/APIKeys.cs
@@ -11,5 +11,19 @@ namespace PopLibrary.UPCDatabaseModels
         public string digitEyesAuthorizationKey { get; set; }
         public string amazonAccessKeyId { get; set; }
         public string amazonSecretAccessKey { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("UPC Database Key: {0}\n" +
+                                 "Digit Eyes App Key: {1}\n" +
+                                 "Digit Eyes Authorization Key: {2}\n" +
+                                 "Amazon Access Key ID: {3}\n" +
+                                 "Amazon Secret Access Key: {4}",
+                                 upcDatabaseKey,
+                                 digitEyesAppKey,
+                                 digitEyesAuthorizationKey,
+                                 amazonAccessKeyId,
+                                 amazonSecretAccessKey);
+        }
     }
 }
--- PopLibrary/PopLibrary.Windows/MainPage.xaml.cs
+++ PopLibrary/PopLibrary.Windows/MainPage.xaml.cs
@@ -4,7 +4,10 @@ using System.Net;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Newtonsoft.Json;
+using Windows.Storage;
 using PopLibrary.DatabasesClasses;
+using PopLibrary.UPCDatabaseModels;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -15,9 +18,8 @@ namespace PopLibrary
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        const string apiKey = "9e233670e5568ae80f24204b76b9aada";
-
         PopLibraryDataManager db;
+        APIKeys keys;
 
         /// <summary>
         /// The constructor for the MainPage UI
@@ -26,6 +28,7 @@ namespace PopLibrary
         {
             this.InitializeComponent();
             db = new PopLibraryDataManager("PopLibrary.db");
+            keys = JsonConvert.DeserializeObject<APIKeys>(ReadConfigFile().Result);
             submitButton.KeyUp += SubmitButton_KeyUp;
             SetupBookList();
         }
@@ -38,7 +41,7 @@ namespace PopLibrary
         private async void SubmitButton_KeyUp(object sender, RoutedEventArgs e)
         {
             // Hit upcdatabase.org for the barcode
-            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://api.upcdatabase.org/json/" + apiKey + "/" + barcodeBox.Text);
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://api.upcdatabase.org/json/" + keys.upcDatabaseKey + "/" + barcodeBox.Text);
             HttpWebResponse res = (HttpWebResponse)(await req.GetResponseAsync());
 
             // Read the response
@@ -75,6 +78,19 @@ namespace PopLibrary
         {
             bookList.ItemsSource = null;
             bookList.ItemsSource = db.QueryAllBooks();
+        }
+
+        /// <summary>
+        /// Reads a local file
+        /// </summary>
+        /// <returns></returns>
+        private async Task<string> ReadConfigFile()
+        {
+            var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var file = await folder.GetFileAsync("config.txt");
+            var contents = await Windows.Storage.FileIO.ReadTextAsync(file);
+            System.Diagnostics.Debug.WriteLine(contents);
+            return contents;
         }
     }
 }
