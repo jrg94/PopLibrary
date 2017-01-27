c0f5fd1a1070dc638903eba13d1eb742552abae4
jrg94 <jrg170@case.edu>
2017-01-27 17:04:20 -0500

    Implemented basic jQuery stuff
    AT LONG LAST! I had been fighting with this for an eternity. Let's get
    to work now.

--- web/pl.js
+++ web/pl.js
@@ -1,11 +1,5 @@
-jQuery(document).ready( function($) {
-
-    $.ajax({
-        // If we want this to be dynamic, we have to localize it through wordpress
-        url: "http://cybrotronics.com",
-        success: function( data ) {
-            alert( 'Your home page has ' + $(data).find('div').length + ' div elements.');
-        }
-    })
-
-})
+function ModifyBGColor(id, newColor)
+{
+  var mElement = document.getElementById(id);
+  mElement.style.backgroundColor = newColor;
+}
--- web/poplibrary-template.php
+++ web/poplibrary-template.php
@@ -6,66 +6,26 @@
      * The basic template file for poplibrary.
      */
 
-    class PopLibrary {
+     // THIS CODE DOESN'T APPEAR TO DO ANYTHING EVEN IN FUNCTIONS.PHP
+     function load_css_js() {
+         wp_enqueue_style( 'pl-style', get_template_directory_uri() . '/css/pl.css');
 
-        /**
-         * Action hooks
-         */
-        public function run() {
-            add_action( 'pop_library', array( $this, 'enqueue_pl_scripts' ));
-            add_action( 'pop_library', array( $this, 'enqueue_pl_styles'));
-            add_action( 'wp_ajax_load_pl', array( $this, 'load_pl'));   // Behavior for logged in users
-            add_action( 'wp_ajax_nopriv_load_pl', array( $this, 'load_pl'));   // Behavior for logged out users
-        }
+         wp_register_script( 'pl-script', get_template_directory_uri() . '/js/pl.js', array('jquery'), NULL, false);
+         wp_localize_script('pl-script', 'myAjax', array(
+                     'ajaxurl' => admin_url('admin-ajax.php')
+                 ));
 
-        /**
-         * Register template styles and scripts
-         */
-        public function register_pl_scripts() {
-            wp_register_script( 'pl-script', get_template_directory_uri() . '/js/pl.js', array('jquery'), null, true);
-            wp_register_style( 'pl-style', get_template_directory_uri() . '/css/pl.css');
-        }
+        wp_enqueue_script( 'pl-script' );
+     }
 
-        /**
-         * Enqueues template-specific scripts
-         */
-        public function enqueue_pl_scripts() {
-            wp_enqueue_script( 'pl-script' );
-        }
+     add_action( 'wp_enqueue_scripts', 'load_css_js');
 
-        /**
-         * Enqueues template-specific styles
-         */
-        public function enqueue_pl_styles() {
-            wp_enqueue_style( 'pl-style' );
-        }
-
-        /**
-         * Loads the entire pop library
-         */
-        public function load_pl() {
-            if ( is_user_logged_in ) {
-                // Open library if user is logged in
-
-                // Grab library information if it exists
-                get_library(wp_get_current_user());
-
-            } else {
-                // Show login box otherwise
-                // Potentially put wp_redirect here for prototype
-                do_shortcode( '[theme-my-login]' );
-            }
-        }
-
-        /**
-         * Generates view based on supplied user
-         */
-        function get_library($pop_user) {
-            echo $pop_user->user_login;
-        }
-    }
-
-    $lib = new PopLibrary;
-    $lib->run();
-
+     wp_head();
 ?>
+<!--<script type="text/javascript" src="https://cybrotronics.com/wp-content/themes/colormag/js/pl.js"></script><div class="dBackgroundBox" id="bgModBox">
+-->
+<p><a id="bgModBox" onclick="ModifyBGColor('bgModBox','#FF0000');">Click here to modify the background color!</a></p>
+</div>
+
+<div id="pl-library">This is the library panel</div>
+<div id="pl-search">This is the search panel</div>
