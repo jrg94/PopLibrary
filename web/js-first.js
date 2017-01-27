2913e3c873af6b0625dc6cf351a4b9dbf62ea2a8
212455708 <jeremy.griffith@ge.com>
2017-01-27 10:25:04 -0500

    Messing arounf in php again

--- web/js-first.js
+++ web/js-first.js
@@ -1,6 +1,7 @@
 jQuery(document).ready( function($) {
 
     $.ajax({
+        // If we want this to be dynamic, we have to localize it through wordpress
         url: "http://cybrotronics.com",
         success: function( data ) {
             alert( 'Your home page has ' + $(data).find('div').length + ' div elements.');
--- web/poplibrary-template.php
+++ web/poplibrary-template.php
@@ -6,11 +6,45 @@
      * The basic template file for poplibrary.
      */
 
+    class PopLibrary {
+
+        /**
+         * Action hooks
+         */
+        public function run() {
+            add_action( 'pop_library', array( $this, 'enqueue_pl_scripts' ));
+            add_action( 'pop_library', array( $this, 'enqueue_pl_styles'));
+        }
+
+        /**
+         * Register template styles and scripts
+         */
+        public function register_pl_scripts() {
+            wp_register_script( 'pl-script', get_template_directory_uri() . '/js/pl.js', array('jquery'), null, true);
+            wp_register_style( 'pl-style', get_template_directory_uri() . '/css/pl.css');
+        }
+
+        /**
+         * Enqueues template-specific scripts
+         */
+        public function enqueue_pl_scripts() {
+            wp_enqueue_script( 'pl-script' );
+        }
+
+        /**
+         * Enqueues template-specific styles
+         */
+        public function enqueue_pl_styles() {
+            wp_enqueue_style( 'pl-style' );
+        }
+    }
+
     if ( is_user_logged_in ) {
         // Open library if user is logged in
 
         // Grab library information if it exists
         get_library(wp_get_current_user());
+        pop_library();
 
     } else {
         // Show login box otherwise
