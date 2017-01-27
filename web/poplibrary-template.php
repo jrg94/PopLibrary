887e6a939e12435297bc9fea5d16a0828fce0c2d
212455708 <jeremy.griffith@ge.com>
2017-01-27 10:32:03 -0500

    Toying with php objects

--- web/poplibrary-template.php
+++ web/poplibrary-template.php
@@ -14,6 +14,7 @@
         public function run() {
             add_action( 'pop_library', array( $this, 'enqueue_pl_scripts' ));
             add_action( 'pop_library', array( $this, 'enqueue_pl_styles'));
+            add_action( 'load_pl', array( $this, 'load_pl'));
         }
 
         /**
@@ -37,24 +38,26 @@
         public function enqueue_pl_styles() {
             wp_enqueue_style( 'pl-style' );
         }
-    }
 
-    if ( is_user_logged_in ) {
-        // Open library if user is logged in
+        public function load_pl() {
+            if ( is_user_logged_in ) {
+                // Open library if user is logged in
 
-        // Grab library information if it exists
-        get_library(wp_get_current_user());
-        pop_library();
+                // Grab library information if it exists
+                get_library(wp_get_current_user());
+                pop_library();
 
-    } else {
-        // Show login box otherwise
-        // Potentially put wp_redirect here for prototype
-        do_shortcode( '[theme-my-login]' );
-    }
+            } else {
+                // Show login box otherwise
+                // Potentially put wp_redirect here for prototype
+                do_shortcode( '[theme-my-login]' );
+            }
+        }
 
-    // Generates view based on supplied user
-    function get_library($pop_user) {
-        echo $pop_user->user_login;
+        // Generates view based on supplied user
+        function get_library($pop_user) {
+            echo $pop_user->user_login;
+        }
     }
 
 ?>
