04af4a241a49f2815723702dd81c5d57cfaeb204
jrg94 <jrg170@case.edu>
2017-01-27 20:30:08 -0500

    Rewriting from scratch
    Alright, I cut out a lot of the enqueue bullshit code and decided to
    write this template from scratch

--- web/poplibrary-template.php
+++ web/poplibrary-template.php
@@ -1,36 +1,14 @@
-<?php
+<head>
+  <link rel="stylesheet" href="https://cybrotronics.com/wp-content/themes/colormag/css/pl.css">
+</head>
 
-    /**
-	 * Template Name: PopLibrary
-	 *
-     * The basic template file for poplibrary.
-     */
+<body>
+  <div class="box">
+    <div id="pl-library">This is the library panel</div>
+    <div id="pl-search">This is the search panel</div>
+  </div>
+</body>
 
-     // THIS CODE DOESN'T APPEAR TO DO ANYTHING EVEN IN FUNCTIONS.PHP
-     function load_css_js() {
-         wp_enqueue_style( 'pl-style', get_template_directory_uri() . '/css/pl.css');
-
-         wp_register_script( 'pl-script', get_template_directory_uri() . '/js/pl.js', array('jquery'), NULL, false);
-         wp_localize_script('pl-script', 'myAjax', array(
-                     'ajaxurl' => admin_url('admin-ajax.php')
-                 ));
-
-        wp_enqueue_script( 'pl-script' );
-     }
-
-     add_action( 'wp_enqueue_scripts', 'load_css_js');
-
-     wp_head();
-
-
-?>
-<!--<script type="text/javascript" src="https://cybrotronics.com/wp-content/themes/colormag/js/pl.js"></script><div class="dBackgroundBox" id="bgModBox">
-<p>
-  <a id="bgModBox" onclick="ModifyBGColor('bgModBox','#FF0000');">Click here to modify the background color!</a>
-</p>
--->
-
-<div class="box">
-  <div id="pl-library">This is the library panel</div>
-  <div id="pl-search">This is the search panel</div>
-</div>
+<footer>
+  <script type="text/javascript" src="https://cybrotronics.com/wp-content/themes/colormag/js/pl.js"></script><div class="dBackgroundBox" id="bgModBox">
+</footer>
