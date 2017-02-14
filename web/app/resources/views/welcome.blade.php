6041e1a5e7107a5f7ef1b5201cd7bd39ce17f4e5
Jeremy <jrg170@case.edu>
2017-02-13 19:04:20 -0500

    Created a books table

--- web/app/resources/views/welcome.blade.php
+++ web/app/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Lebron
                 </div>
 
                 <div class="links">
