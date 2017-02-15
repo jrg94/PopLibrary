73c49144a22371598b01eb405a23a4397f907093
Jeremy <jrg170@case.edu>
2017-02-14 20:33:27 -0500

    Creating the basic tables

--- web/app/database/migrations/2017_02_15_012816_create_user_books_table.php
+++ web/app/database/migrations/2017_02_15_012816_create_user_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateBooksTable extends Migration
+class CreateUserBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,8 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('user_books', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
-            $table->string('isbn');
-            $table->string('asin');
             $table->timestamps();
         });
     }
@@ -29,6 +26,6 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('user_books');
     }
 }
