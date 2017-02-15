1921f1bd60d61e5b6deca3ca77ddde61e97e0c52
Jeremy <jrg170@case.edu>
2017-02-14 20:49:06 -0500

    Added the many to many relationship on the user end

--- web/app/app/User.php
+++ web/app/app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function books()
+    {
+        return $this->hasMany(Book::class);
+    }
 }
