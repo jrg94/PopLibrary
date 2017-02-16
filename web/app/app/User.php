993a4d9a7ea4a3d45995963bdbf7658743161e1a
Jeremy <jrg170@case.edu>
2017-02-15 22:35:39 -0500

    Trying to discern between my loans in/out

--- web/app/app/User.php
+++ web/app/app/User.php
@@ -27,8 +27,29 @@ class User extends Authenticatable
         'password', 'remember_token',
     ];
 
+    /**
+     * Retrieves any books this user might have.
+     */
     public function books()
     {
         return $this->belongsToMany(Book::class);
+    }
+
+    /**
+     * Retrieves all the loans this user has lent.
+     * TODO: Come up with a name that makes sense.
+     */
+    public function loans()
+    {
+        return $this->hasMany(Loan::class, 'user_id', 'id');
+    }
+
+    /**
+     * Retrieves all the loans this user has received.
+     * TODO: Come up with a name that makes sense.
+     */
+    public function borrows()
+    {
+        return $this->hasMany(Loan::class, 'borrower_id', 'id');
     }
 }
