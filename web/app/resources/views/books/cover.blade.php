2b817624cf665e5fbdf446a2fcc60e6a8ce1860e
Jeremy <jrg170@case.edu>
2017-02-18 00:34:06 -0500

    Experimenting with css
    Learning how to use Laravel Elixer

---
+++ web/app/resources/views/books/cover.blade.php
@@ -0,0 +1,7 @@
+<div class="col-md-3">
+    <div class="panel-body">
+        <a href="/books/{{ $book->id }}">
+            <img  src="https://marketplace.canva.com/MAB___U-clw/1/0/thumbnail_large/canva-yellow-lemon-children-book-cover-MAB___U-clw.jpg" class="img-thumbnail">
+        </a>
+    </div>
+</div>
--- web/app/resources/views/home.blade.php
+++ web/app/resources/views/home.blade.php
@@ -3,15 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-3 no-float">
             <div class="panel panel-default">
                 <div class="panel-heading">Filters</div>
                 <div class="panel-body">
                     All
                 </div>
             </div>
+
+            <div class="panel panel-default">
+                <div class="panel-heading">Add a Book</div>
+                <div class="panel-body">
+                    @include ('books.add')
+                </div>
+            </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-9 no-float">
             <div class="panel panel-default">
                 <div class="panel-heading">Library</div>
 
@@ -27,15 +34,6 @@
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-3">
-            <div class="panel panel-default">
-                <div class="panel-heading">Add a Book</div>
-                <div class="panel-body">
-                    @include ('books.add')
-                </div>
-            </div>
-        </div>
-    </div>
+
 </div>
 @endsection
