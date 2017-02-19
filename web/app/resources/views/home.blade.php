b98e577a17f3ffae5ca919df35b0ac5c944a623b
Jeremy <jrg170@case.edu>
2017-02-19 02:55:59 -0500

    Fixed up a UI issue

--- web/app/resources/views/home.blade.php
+++ web/app/resources/views/home.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.poplibrary')
 
 @section('dashboard')
 
--- web/app/resources/views/layouts/app.blade.php
+++ web/app/resources/views/layouts/app.blade.php
@@ -78,20 +78,8 @@
             </div>
         </nav>
 
-        <div class="container">
-            <div class="row">
-                <div class="col-md-3">
-                    <div class="panel panel-default">
-                        @yield('dashboard')
-                    </div>
-                </div>
-                <div class="col-md-9">
-                    <div class="panel panel-default">
-                        @yield('library')
-                    </div>
-                </div>
-            </div>
-        </div>
+        @yield('content')
+
     </div>
 
     <!-- Scripts -->
