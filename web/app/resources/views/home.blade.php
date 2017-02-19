b199c5bc5701b48cb77c83c79d443af883ab11d2
Jeremy <jrg170@case.edu>
2017-02-18 20:57:55 -0500

    Fit the app in the window

--- web/app/resources/views/home.blade.php
+++ web/app/resources/views/home.blade.php
@@ -3,22 +3,15 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-3 no-float">
+        <div class="col-md-3">
             <div class="panel panel-default">
                 <div class="panel-heading">Filters</div>
                 <div class="panel-body">
                     All
                 </div>
             </div>
-
-            <div class="panel panel-default">
-                <div class="panel-heading">Add a Book</div>
-                <div class="panel-body">
-                    @include ('books.add')
-                </div>
-            </div>
         </div>
-        <div class="col-md-9 no-float">
+        <div class="col-md-9">
             <div class="panel panel-default">
                 <div class="panel-heading">Library</div>
 
