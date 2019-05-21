e819780aa5a2cbe0d473c3839a5f60db72e92b3f
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-05-20 21:19:39 -0400

    Toying with parameters

--- mobile/app/src/main/java/com/example/poplibrary/Book.kt
+++ mobile/app/src/main/java/com/example/poplibrary/Book.kt
@@ -1,7 +1,11 @@
 package com.example.poplibrary
 
-class Book {
-    var ISBN: String = ""
+import java.util.*
+
+class Book(val ISBN: String) {
     var title: String = ""
     var pageCount: Int = -1
+    var language: String = ""
+    var editor: String = ""
+    var dateOfPublication: Date = Date()
 }
