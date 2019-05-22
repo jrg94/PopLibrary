d127ddaeaa7a62990d383d0930cdedf42c9011a9
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-05-22 12:00:10 -0400

    Toying with editing text

--- mobile/app/src/main/java/com/example/poplibrary/MainActivity.kt
+++ mobile/app/src/main/java/com/example/poplibrary/MainActivity.kt
@@ -2,12 +2,19 @@ package com.example.poplibrary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //Book b = Book("174-44-3221")
+
+        val book = Book("174-44-3221")
+        book.pageCount = 100
+
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = book.pageCount.toString()
+        }
     }
 }
