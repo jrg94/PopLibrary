452d7b233c615a4bcf9c7801336f24a12b679d2f
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-05-22 14:16:37 -0400

    Expanding recyclerview

--- mobile/app/src/main/java/com/example/poplibrary/BookAdapter.kt
+++ mobile/app/src/main/java/com/example/poplibrary/BookAdapter.kt
@@ -4,11 +4,13 @@ import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import android.view.LayoutInflater
+import android.widget.TextView
 import kotlinx.android.synthetic.main.book_list_item.view.*
 
 class BookAdapter (private val books: Array<Book>) : RecyclerView.Adapter<BookAdapter.BookViewHolder>() {
     class BookViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-        val isbnTextView = view.isbn_text_view
+        val isbnTextView: TextView = view.isbn_text_view
+        val titleTextView: TextView = view.title_text_view
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookAdapter.BookViewHolder {
@@ -20,6 +22,7 @@ class BookAdapter (private val books: Array<Book>) : RecyclerView.Adapter<BookAd
 
     override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
         holder.isbnTextView.text = books[position].ISBN
+        holder.titleTextView.text = books[position].title
     }
 
     override fun getItemCount() = books.size
