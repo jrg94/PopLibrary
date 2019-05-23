068aeac89ad139c7fc11ae18aee2930bbedba964
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-05-23 13:09:28 -0400

    Created a new activity

---
+++ mobile/app/src/main/java/com/example/poplibrary/BookActivity.kt
@@ -0,0 +1,11 @@
+package com.example.poplibrary
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class BookActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.book_add_form)
+    }
+}
