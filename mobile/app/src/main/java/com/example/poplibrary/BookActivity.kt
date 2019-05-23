673164da3253b508369ac35486ecfdaf35931b97
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-05-23 13:39:21 -0400

    Added return in book add activity

--- mobile/app/src/main/java/com/example/poplibrary/BookActivity.kt
+++ mobile/app/src/main/java/com/example/poplibrary/BookActivity.kt
@@ -1,11 +1,18 @@
 package com.example.poplibrary
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class BookActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.book_add_form)
+    }
+
+    fun loadMainActivity(view: View) {
+        val intent: Intent = Intent(this, MainActivity::class.java)
+        this.startActivity(intent)
     }
 }
