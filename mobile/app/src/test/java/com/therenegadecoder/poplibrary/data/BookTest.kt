ebda116909d0264b1e4c174e5dfb3dee8b3d45d7
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-05-25 01:31:33 -0400

    I lied, Travis does perform tests

--- mobile/app/src/test/java/com/therenegadecoder/poplibrary/data/BookTest.kt
+++ mobile/app/src/test/java/com/therenegadecoder/poplibrary/data/BookTest.kt
@@ -31,7 +31,7 @@ class BookTest {
     @org.junit.Test
     fun matchISBN13() {
         val book = Book(isbn13 = "976-143136645")
-        assertFalse(book.match("66"))
+        assertTrue(book.match("66"))
     }
 
     @org.junit.Test
