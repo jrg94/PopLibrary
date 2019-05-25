867221926196fdfc05e9b4f2054615964b5e0de5
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-05-24 20:51:35 -0400

    Added several other match tests

--- mobile/app/src/test/java/com/therenegadecoder/poplibrary/data/BookTest.kt
+++ mobile/app/src/test/java/com/therenegadecoder/poplibrary/data/BookTest.kt
@@ -7,13 +7,31 @@ class BookTest {
     @org.junit.Test
     fun matchTitle() {
         val book = Book(title = "Hello, World!")
-        assertTrue(book.match("Hello"))
+        assertTrue("Failed to match ${book.title} to Hello", book.match("Hello"))
     }
-    
+
     @org.junit.Test
     fun matchAuthor() {
         val book = Book(author = "James Earl Jones")
-        assertTrue(book.match("Earl"))
+        assertTrue("Failed to match ${book.author} to Earl", book.match("Earl"))
+    }
+
+    @org.junit.Test
+    fun matchEditor() {
+        val book = Book(editor = "The Pittsburgh Penguins")
+        assertTrue(book.match("Penguin"))
+    }
+
+    @org.junit.Test
+    fun matchLanguage() {
+        val book = Book(language = "English")
+        assertTrue(book.match("English"))
+    }
+
+    @org.junit.Test
+    fun matchNone() {
+        val book = Book(title = "The Great Gatsby", language = "English", author = "Stephen Colbert")
+        assertFalse(book.match("Art"))
     }
 
 }
