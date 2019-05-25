b6d5414388f6bacdb92c68e38a73db5e7817edaa
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-05-24 21:10:31 -0400

    Added several lexile unit tests

--- mobile/app/src/test/java/com/therenegadecoder/poplibrary/data/LexileTest.kt
+++ mobile/app/src/test/java/com/therenegadecoder/poplibrary/data/LexileTest.kt
@@ -13,6 +13,28 @@ class LexileTest {
     }
 
     @Test
-    fun compareTo() {
+    fun toStringAD() {
+        val lex = Lexile(1200, Lexile.LexileType.AD)
+        assertEquals("AD1200L", lex.toString())
+    }
+
+    @Test
+    fun toStringNA() {
+        val lex = Lexile(194, Lexile.LexileType.NA)
+        assertEquals("194L", lex.toString())
+    }
+
+    @Test
+    fun compareToPositive() {
+        val lex1 = Lexile(270, Lexile.LexileType.NA)
+        val lex2 = Lexile(400, Lexile.LexileType.NA)
+        assertTrue(lex1 < lex2)
+    }
+
+    @Test
+    fun compareToNegative() {
+        val lex1 = Lexile(270, Lexile.LexileType.NA)
+        val lex2 = Lexile(400, Lexile.LexileType.BR)
+        assertTrue(lex2 < lex1)
     }
 }
